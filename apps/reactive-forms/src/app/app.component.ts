04f0740b0f2c173a1363f0deaaf14cf67fa62b29
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-11-08 19:41:02 +0100

    responsive forms testing in progress

--- apps/reactive-forms/src/app/app.component.ts
+++ apps/reactive-forms/src/app/app.component.ts
@@ -7,23 +7,42 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  // // public username = new FormControl();
-  // // public password = new FormControl();
-  //
   // // loginForm = new FormGroup({
   // //   username: new FormControl(''),
   // //   password: new FormControl(''),
   // // });
-  //
+
+  passwordRegularExpression =
+    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+  showPassword = false;
 
   loginForm = this.formBuilder.group({
-    username: ['', [Validators.required, Validators.minLength(4)]],
-    password: ['', [Validators.required, Validators.minLength(6)]],
+    username: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.pattern('[a-zA-Z ]*'),
+      ],
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(16),
+        Validators.pattern(this.passwordRegularExpression),
+      ],
+    ],
   });
 
   constructor(private formBuilder: FormBuilder) {}
 
   submit() {
     alert(`form submitted`);
+  }
+
+  toggelShowPassword() {
+    this.showPassword = !this.showPassword;
   }
 }

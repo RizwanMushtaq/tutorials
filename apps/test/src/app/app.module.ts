51d4eae775b6043b01c8bafc81c97eb9b86c39aa
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-11-09 12:44:46 +0100

    getting ready for tutorial

--- apps/test/src/app/app.module.ts
+++ apps/test/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { ResponsiveFormTestingComponent } from './responsive-form-testing/responsive-form-testing.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularReactiveLoginFormCompleteComponent } from './angular-reactive-login-form-complete/angular-reactive-login-form-complete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NxWelcomeComponent,
     ResponsiveFormTestingComponent,
+    AngularReactiveLoginFormCompleteComponent,
   ],
   imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],

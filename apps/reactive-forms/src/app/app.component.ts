0b49107033e9210c7f190a8946b28ab50fb850cf
Rizwan Mushtaq <rizwan.mushtaq@ottonova.de>
2022-11-09 13:50:01 +0100

    basic tutorial recording done

--- apps/reactive-forms/src/app/app.component.ts
+++ apps/reactive-forms/src/app/app.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'tutorials-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  // username = new FormControl('');
+  // password = new FormControl('');
+
+  // loginForm = new FormGroup({
+  //   username: new FormControl(''),
+  //   password: new FormControl(''),
+  // });
+
+  loginForm = this.formBuilder.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  submit() {
+    console.log(this.loginForm.controls.username);
+    alert(`form submitted ${this.loginForm.controls.username.value}`);
+  }
+}
